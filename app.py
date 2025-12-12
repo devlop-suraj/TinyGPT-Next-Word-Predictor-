35027262a750e5ac85a8be5467b3c565c35269a3
Suraj Kumar <126854944+devlop-suraj@users.noreply.github.com>
2025-12-12 20:23:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,130 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import random
+
+from transformer_block import Block
+
+
+print("Torch version:", torch.__version__)
+print("CUDA available:", torch.cuda.is_available())
+print("GPU name:", torch.cuda.get_device_name(0) if torch.cuda.is_available() else "None")
+
+def load_corpus(file_name="corpus.txt"):
+    """
+    Loads training sentences from a text file.
+    Each line in the file becomes one string in the list.
+    """
+    try:
+        with open(file_name, "r", encoding="utf-8") as f:
+            corpus = [line.strip() for line in f if line.strip()]
+        return corpus
+    except FileNotFoundError:
+        print(f"Error: The file '{file_name}' was not found.")
+        return []
+    
+
+
+corpus = load_corpus()
+
+corpus = [s + " <END>" for s in corpus]
+text = " ".join(corpus)
+#print (text)
+
+words = list(set(text.split()))
+#print(words)
+
+vocab_size = len(words)
+#print(vocab_size)
+
+word2idx = {w: i for i, w in enumerate(words)}
+#print("word2idx : ", word2idx)
+
+idx2word = {i: w for w, i in word2idx.items()} 
+#print("idx2word : ", idx2word) 
+
+data = torch.tensor([word2idx[w] for w in text.split()], dtype=torch.long)
+#print("data : ", data) 
+#print(len(data))
+
+block_size = 6 #it means whenever our model is predicting the next word the model will see how much previous word
+embedding_dim = 32 #32 values for each word means dimension.
+n_heads = 2  #self attention heads
+n_layers = 2 
+lr = 1e-3 #learning rate
+epochs = 1500  
+
+
+def get_batch(batch_size=16):
+    ix = torch.randint(len(data) - block_size, (batch_size,))  
+    x = torch.stack([data[i:i+block_size] for i in ix])  
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix]) 
+    return x, y
+
+
+
+
+class TinyGPT(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.token_embedding = nn.Embedding(vocab_size, embedding_dim) 
+
+        self.position_embedding = nn.Embedding(block_size, embedding_dim) 
+        self.blocks = nn.Sequential(*[Block(embedding_dim, block_size, n_heads) for _ in range(n_layers)]) 
+
+        self.ln_f = nn.LayerNorm(embedding_dim)
+        self.head = nn.Linear(embedding_dim, vocab_size) 
+
+    def forward(self, idx, targets=None):
+        B, T = idx.shape 
+        tok_emb = self.token_embedding(idx) 
+        
+        pos_emb = self.position_embedding(torch.arange(T, device=idx.device))
+        x = tok_emb + pos_emb  
+        x = self.blocks(x) 
+        x = self.ln_f(x)
+        logits = self.head(x) 
+        loss = None
+        if targets is not None:
+            B, T, C = logits.shape 
+            loss = F.cross_entropy(logits.view(B*T, C), targets.view(B*T)) 
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            idx_cond = idx[:, -block_size:]
+            logits, _ = self(idx_cond)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=-1)
+            next_idx = torch.multinomial(probs, 1)
+            idx = torch.cat((idx, next_idx), dim=1)
+        return idx
+
+
+
+model = TinyGPT()
+optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+
+for step in range(epochs):
+    xb, yb = get_batch() 
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    if step % 300 == 0:
+        print(f"Step {step}, loss={loss.item():.4f}")
+
+
+start_word = input("Enter a starting word: ")
+if start_word in word2idx:
+    context = torch.tensor([[word2idx[start_word]]], dtype=torch.long)
+else:
+    print(f"Error: '{start_word}' is not in the vocabulary.")
+    exit()
+
+
+
+out = model.generate(context, max_new_tokens=10)
+
+print("\nGenerated text:\n")
+print(" ".join(idx2word[int(i)] for i in out[0]))
